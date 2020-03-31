d27e17a1840d0470b488bc05bb8384c311e74a79
mkamranhamid <mkamranhamid@gmail.com>
2020-03-31 20:24:43 +0500

    initial commit

---
+++ created_by_bash.md
@@ -0,0 +1,2 @@
+## Meme Adder on push
+This project is a small demo of how and what you can do with the bash scripts. Using github action everytime action is triggered this bash script will run and add a random gif at the end.
---
+++ index.sh
@@ -0,0 +1,11 @@
+file_name=./created_by_bash.md
+response_url=$(curl -s https://meme-api.herokuapp.com/gimme | jq -r '"### `"+.title+"`\n"+"!["+.title+"](" + .url +")"')
+
+if [[ -z "$response_url" ]]; then
+   $response_url="https://media.giphy.com/media/YyKPbc5OOTSQE/giphy.gif"
+fi
+
+printf "\n$response_url" >> $file_name
+
+echo $response_url
+# curl -s "http://api.icndb.com/jokes/random" | jq '.value.joke'
